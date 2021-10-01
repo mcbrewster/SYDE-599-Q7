455c29b0fc5bce5d8328ea27589da0e9be1539a6
Margaret Brewster <margaretbrewster@Margarets-MacBook-Air.local>
2021-10-01 12:37:19 -0400

    first draft

---
+++ DNAReader.java
@@ -0,0 +1,43 @@
+public class DNAReader {
+    private static String templateStrand = "";
+    private static String complmentaryStrand = "";
+    private static String mRNA = "";
+
+    public static void main(String args[]) {
+        if(args.length < 1){
+            System.out.println("please privide a template strand");
+            System.exit(0); 
+        }
+        templateStrand = args[0]; 
+        
+        for(int i = 0; i < templateStrand.length(); i++){
+            char current = templateStrand.charAt(i); 
+            switch(current) {
+                case 'A':
+                complmentaryStrand += "T";
+                mRNA += "U";
+                  break;
+                case 'C':
+                complmentaryStrand += "G";
+                mRNA += "G";
+                  break;
+                case 'T':
+                complmentaryStrand += "A";
+                mRNA += "A";
+                break;
+                case 'G':
+                complmentaryStrand += "C";
+                mRNA += "C";
+                  break;
+                default:
+                  // do nothing
+              }
+        }
+
+        System.out.println("Complimentary Strand: " + complmentaryStrand);
+        System.out.println("mRNA: " + mRNA);
+
+
+    }
+}
+
